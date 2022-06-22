2d1223a9f9b8db434053275a2024629ba5f48ea1
gagneja1512 <aadityaarora1215@gmail.com>
2022-06-22 18:57:56 +0530

    Cycle Detection Using DSU

---
+++ Cycle Detection Using DSU.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Graph{
+	int v;
+	list<pair<int , int>> l;
+
+public:
+	Graph(int value)
+	{
+		v = value;
+	}	
+
+	void addEdge(int x , int y)
+	{
+		l.push_back(make_pair(x , y));
+	}
+
+	int find_set(int i , int parent[])
+	{
+		if(parent[i] == -1)
+			return i ;
+
+		return find_set(parent[i] , parent);	
+	}
+
+	void union_set(int a , int b , int parent[])
+	{
+		int s1 = find_set(a , parent);
+		int s2 = find_set(b , parent);
+
+		if(s1 != s2)
+			parent[s2] = s1;
+	}
+
+	bool contains_cycle()
+	{
+		int *parent = new int[v];
+
+		for(int i=0 ; i<v ; i++)
+			parent[i] = -1;
+
+		for(auto edge : l)
+		{
+			int i = edge.first ;
+			int j = edge.second ;
+
+			int s1 = find_set(i , parent);
+			int s2 = find_set(j , parent);
+
+			if(s1 != s2)
+				union_set(s1 , s2 , parent);
+
+			else 
+				return true ;	
+		}
+
+		return false ;
+	}
+};
+
+int main()
+{
+	Graph g(4);
+	g.addEdge(0 , 1);
+	g.addEdge(1 , 2);
+	g.addEdge(2 , 3);
+	g.addEdge(3 , 1);
+
+	cout<<g.contains_cycle()<<endl;
+	return 0;
+}
