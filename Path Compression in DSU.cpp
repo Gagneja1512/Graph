df41825ab0c93ff23714413c4f2ca54687a1b86b
gagneja1512 <aadityaarora1215@gmail.com>
2022-06-24 23:40:05 +0530

    Path Compression

---
+++ Path Compression in DSU.cpp
@@ -0,0 +1,98 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Graph{
+	int v;
+	list<pair<int , int>> l;
+
+public:
+	Graph(int value)
+	{
+		v = value;
+	}	
+
+	void addEdge(int x , int y)
+	{
+		l.push_back(make_pair(x , y));
+	}
+
+	int find_set(int i , int parent[])
+	{
+		if(parent[i] == -1)
+			return i ;
+
+		//Path compression technique
+		return parent[i] = find_set(parent[i] , parent);	
+	}
+
+	void union_set(int a , int b , int parent[] , int rank[])
+	{
+		int s1 = find_set(a , parent);
+		int s2 = find_set(b , parent);
+
+		if(s1 != s2)
+		{	
+			//Union By Rank
+			if(rank[s1] <= rank[s2])
+			{
+				parent[s1] = s2;
+				rank[s2] = rank[s2] + rank[s1];
+			}
+			else
+			{
+				parent[s2] = s1;
+				rank[s1] = rank[s1] + rank[s2];
+			}
+		}
+	}
+
+	bool contains_cycle()
+	{
+		int *parent = new int[v];
+		int *rank = new int[v]; 
+
+		for(int i=0 ; i<v ; i++)
+		{
+			parent[i] = -1;
+			rank[i] = 1;
+		}	
+		for(auto edge : l)
+		{
+			int i = edge.first ;
+			int j = edge.second ;
+
+			int s1 = find_set(i , parent);
+			int s2 = find_set(j , parent);
+
+			if(s1 != s2)
+				union_set(s1 , s2 , parent, rank);
+
+			else 
+			{
+				for(auto k = 0 ; k<v ; k++)
+					cout<<parent[k]<<" ";
+				
+				cout<<endl;
+				return true ;
+			}
+		}
+
+		for(int k = 0 ; k<v ; k++)
+			cout<<parent[k]<<" ";
+				
+		cout<<endl;
+		return false ;
+	}
+};
+
+int main()
+{
+	Graph g(4);
+	g.addEdge(0 , 1);
+	g.addEdge(1 , 2);
+	g.addEdge(2 , 3);
+	g.addEdge(3 , 1);
+
+	cout<<g.contains_cycle()<<endl;
+	return 0;
+}
