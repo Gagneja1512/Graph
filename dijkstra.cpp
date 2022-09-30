0942e94bdc25810fd97a57ebdb8b137c5b127d03
gagneja1512 <aadityaarora1215@gmail.com>
2022-09-30 12:59:41 +0530

    Dijkstra Edited

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -1,75 +1,63 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-class Graph
-{
-	int vertex;
-	list<pair<int , int>> *l;
-
-	public:
-	Graph(int vertices)
-	{
-		vertex = vertices ;
-		l = new list<pair<int , int>>[vertices];
-	}
-
-	void addEdge(int u , int v , int wt , bool undir = true)
-	{
-		l[u].push_back({wt , v});
-		if(undir)
-			l[v].push_back({wt , u});
-	}
-
-	int dijkstra(int src , int dest)
-	{
-		vector<int> dis(vertex , INT_MAX);
-		set<pair<int , int>> s;
-
-		dis[src] = 0;
-		s.insert({0,src});
-
-		while(!s.empty())
-		{
-			auto it = s.begin();
-			int node = it->second ;
-			int disTillNow = it->first ;
-			s.erase(it) ;
-
-			for(auto nbrPair : l[node])
-			{
-				int nbr = nbrPair.second ;
-				int current = nbrPair.first ;
-
-				if(current + disTillNow < dis[nbr])
-				{
-					auto f = s.find({dis[nbr] , nbr});
-					if(f != s.end())
-						s.erase(f);
-
-					dis[nbr] = current + disTillNow ;
-					s.insert({dis[nbr] , nbr});	
-				}
-			}
-		}
-
-		for(int i=0 ; i<vertex ; i++)
-		{
-			cout<<src<<" -> distance -> "<<dis[i]<<endl;
-		}
-
-		return dis[dest];
-	}
+class Solution {
+public:
+    int dijkstra(int source , int n , vector<pair<int , int>> g[])
+    {
+        priority_queue<pair<int , int> , vector<pair<int , int>> , greater<pair<int , int>>> pq ;
+        
+        vector<int> distance(n+1 , INT_MAX) ;
+        vector<bool> visited(n+1 , false) ; 
+        
+        distance[source] = 0 ;
+        pq.push({distance[source] , source}) ;
+        
+        while(pq.size() > 0)
+        {   
+            auto node = pq.top() ;
+            int v = node.second ;
+            int vdis = node.first ;
+                
+            pq.pop() ;
+            
+            if(visited[v])
+                continue ;
+            visited[v] = true ;
+            for(auto child : g[v])
+            {
+                int child_v = child.first ;
+                int child_d = child.second ;
+                
+                if(child_d + distance[v] < distance[child_v])
+                {
+                    distance[child_v] = child_d + distance[v];
+                    pq.push({distance[child_v] , child_v}) ;
+                }
+            }
+        }
+        
+        int ans = INT_MIN ;
+        for(int i=1 ; i<=n ; i++)
+        {
+            if(distance[i] == INT_MAX)
+            {
+                return -1 ;
+            }
+            
+            //cout<<distance[i]<<' ' ;
+            ans = max(ans , distance[i]) ;
+        }
+        
+        return ans  ;
+    }
+    
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        vector<pair<int , int>> g[n+1] ;
+        
+        int sz = times.size() ;
+        for(int i=0 ; i<sz ; i++)
+        {
+            g[times[i][0]].push_back({times[i][1] , times[i][2]}) ;
+        }
+        
+        return dijkstra(k , n , g) ;
+    }
 };
-
-int main()
-{
-	Graph g(5);
-	g.addEdge(0 , 1 , 1);
-	g.addEdge(1 , 2 , 1);
-	g.addEdge(0 , 2 , 4);
-	g.addEdge(0 , 3 , 7);
-	g.addEdge(3 , 2 , 2);
-	g.addEdge(3 , 4 , 3);
-
-	cout<<g.dijkstra(0 , 3)<<endl;
-}
